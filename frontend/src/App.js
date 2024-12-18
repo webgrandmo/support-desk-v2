aa113de32e7396a2d0da358b83801449f694ef73
Andriy Trofimov <webgrandmo@gmail.com>
2024-12-18 21:13:16 +0200

    Add header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,33 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Header from './components/layout/Header';
+
 function App() {
-	return <div>My app</div>;
+	return (
+		<>
+			<Router>
+				<div className='container'>
+					<Header />
+					<Routes>
+						<Route
+							path='/'
+							element={<Home />}
+						/>
+						<Route
+							path='/login'
+							element={<Login />}
+						/>
+						<Route
+							path='/register'
+							element={<Register />}
+						/>
+					</Routes>
+				</div>
+			</Router>
+		</>
+	);
 }
 
 export default App;
